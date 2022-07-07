1e20d57c6cbf73557ee18877c77a066669a84481
Gilvan Ferreira <gilvanfcf77gmail.com>
2022-07-07 01:33:22 -0300

    refresh list

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from 'react';
 import PostItems from '../../components/postItems';
 import HeaderForMobile from '../../components/headerForMobile';
-import { FlatList } from 'react-native';
+import { FlatList, RefreshControl } from 'react-native';
 import { useGlobal } from 'reactn';
 import { fetchAllPosts } from '../../services/Posts';
 
 const Home = () => {
+    const wait = (timeout) => {
+        return new Promise(resolve => setTimeout(resolve, timeout));
+    }
+
+    const [refreshing, setRefreshing] = React.useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+
     const [category] = useGlobal('category');
     const [posts] = useGlobal('posts');
 
@@ -32,6 +43,12 @@ const Home = () => {
                         filteredList
                 }
                 renderItem={({ item }) => <PostItems post={item} />}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
             />
         </>
     );
